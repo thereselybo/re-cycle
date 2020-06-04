45d8fa65e4959139ca3e894895580192e99f5476
Therese Lybo <therese.lybo@gmail.com>
2020-06-04 19:35:08 +0200

    Added loading spinner and formatted files

--- js/contact.js
+++ js/contact.js
@@ -3,75 +3,76 @@ const contactForm = document.querySelector("#contact_form");
 contactForm.addEventListener("submit", validateForm);
 
 function validateForm(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const firstNameValue = document.querySelector("#firstName").value;
-    // const firstNameValue = firstName.value;
-    const firstNameError = document.querySelector("#firstNameError");
+  const firstNameValue = document.querySelector("#firstName").value;
+  const firstNameError = document.querySelector("#firstNameError");
 
-    if(checkLength(firstNameValue)) {
-        firstNameError.style.display = "none";
+  if (checkLength(firstNameValue)) {
+    firstNameError.style.display = "none";
+  } else {
+    firstNameError.style.display = "block";
+  }
+
+  const lastNameValue = document.querySelector("#lastName").value;
+  const lastNameError = document.querySelector("#lastNameError");
+
+  if (checkLength(lastNameValue)) {
+    lastNameError.style.display = "none";
+  } else {
+    lastNameError.style.display = "block";
+  }
+
+  const emailValue = document.querySelector("#email").value;
+  const noEmailError = document.querySelector("#noEmailError");
+  const invalidEmailError = document.querySelector("#invalidEmailError");
+
+  if (checkLength(emailValue)) {
+    noEmailError.style.display = "none";
+    if (validateEmail(emailValue)) {
+      invalidEmailError.style.display = "none";
     } else {
-        firstNameError.style.display = "block";
+      invalidEmailError.style.display = "block";
     }
+  } else {
+    noEmailError.style.display = "block";
+  }
 
-    const lastNameValue = document.querySelector("#lastName").value;
-    const lastNameError = document.querySelector("#lastNameError");
+  const messageValue = document.querySelector("#message").value;
+  const messageError = document.querySelector("#messageError");
 
-    if(checkLength(lastNameValue)) {
-        lastNameError.style.display = "none";
-    } else {
-        lastNameError.style.display = "block";
-    }
+  if (checkLength(messageValue)) {
+    messageError.style.display = "none";
+  } else {
+    messageError.style.display = "block";
+  }
 
-    const emailValue = document.querySelector("#email").value;
-    const noEmailError = document.querySelector("#noEmailError");
-    const invalidEmailError = document.querySelector("#invalidEmailError");
+  const validated = document.querySelector("#formValidated");
 
-    if(checkLength(emailValue)) {
-        noEmailError.style.display = "none";
-        if(validateEmail(emailValue)) {
-            invalidEmailError.style.display = "none";
-        } else {
-            invalidEmailError.style.display = "block";
-        }
-    } else {
-        noEmailError.style.display = "block";
-    }
-
-    const messageValue = document.querySelector("#message").value;
-    const messageError = document.querySelector("#messageError");
-
-    if(checkLength(messageValue)) {
-        messageError.style.display = "none";
-    } else {
-        messageError.style.display = "block";
-    }
-
-    const validated = document.querySelector("#formValidated");
-
-    if((firstNameError.style.display &&
-        lastNameError.style.display &&
-        noEmailError.style.display &&
-        invalidEmailError.style.display &&
-        messageError.style.display) === "none") {
-            validated.style.display = "block";
-        } else {
-            validated.style.display = "none";
-        }
+  if (
+    (firstNameError.style.display &&
+      lastNameError.style.display &&
+      noEmailError.style.display &&
+      invalidEmailError.style.display &&
+      messageError.style.display) === "none"
+  ) {
+    validated.style.display = "block";
+  } else {
+    validated.style.display = "none";
+  }
 }
 
 function checkLength(value) {
-    const trimmedValue = value.trim();
+  const trimmedValue = value.trim();
 
-    if(trimmedValue) {
-        return true;
-    } else {
-        return false;
-    }
+  if (trimmedValue) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function validateEmail(email) {
-    const regEx = /\S+@\S+\.\S+/;
-    return regEx.test(email);
+  const regEx = /\S+@\S+\.\S+/;
+  return regEx.test(email);
 }
--- js/nav.js
+++ js/nav.js
@@ -3,41 +3,29 @@ const hamburger = document.querySelector("#hamburger");
 const cross = document.querySelector("#cross");
 
 hamburger.addEventListener("click", function openNav() {
-    hamburger.style.display = "none";
-    cross.style.display = "block";
-    nav.style.width = "45vw";
-})
+  hamburger.style.display = "none";
+  cross.style.display = "block";
+  nav.style.width = "45vw";
+});
 
 cross.addEventListener("click", function closeNav() {
-    cross.style.display = "none";
-    hamburger.style.display = "block";
-    nav.style.width = 0;
-})
-
-// if(window.matchMedia("(min-width: 1024px)")) {
-//     nav.style.width = "40%";
-// } else {
-//     nav.style.width = 0;
-// }
-
-// if(window.innerWidth > 1023 || document.documentElement.clientWidth > 1023) {
-//     nav.style.width = "40%";
-// } else {
-//     nav.style.width = 0;
-// }
+  cross.style.display = "none";
+  hamburger.style.display = "block";
+  nav.style.width = 0;
+});
 
 const breakpoint = window.matchMedia("(min-width: 1024px)");
 
 function checkWindowSize(breakpoint) {
-    if(breakpoint.matches) {
-        nav.style.width = "500px";
-        hamburger.style.display = "none";
-        cross.style.display = "none";
-    } else {
-        nav.style.width = 0;
-        hamburger.style.display = "block";
-    }
+  if (breakpoint.matches) {
+    nav.style.width = "500px";
+    hamburger.style.display = "none";
+    cross.style.display = "none";
+  } else {
+    nav.style.width = 0;
+    hamburger.style.display = "block";
+  }
 }
 
 checkWindowSize(breakpoint);
-breakpoint.addListener(checkWindowSize)
+breakpoint.addListener(checkWindowSize);
